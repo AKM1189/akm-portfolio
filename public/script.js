51d96bf032ff42b25c13dd3519693354089cb35a
akm <akm@gmail.com>
2024-11-14 00:11:39 +0530

    my-portfolio

---
+++ public/script.js
@@ -0,0 +1,137 @@
+$(".dark").show();
+$(".light").hide();
+
+var app = document.querySelector(".role");
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(500)
+  .typeString("FRONTEND DEVELOPER")
+  .pauseFor(1000)
+  .deleteChars(18)
+  .pauseFor(500)
+  .typeString("BACKEND DEVELOPER")
+  .pauseFor(1000)
+  .deleteChars(17)
+  //   .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+  .start();
+
+//   if($('.content').hasClass('bg-gradient-light')) {
+//   }
+//   else {
+//     $('.dark').hide();
+//     $('.light').show();
+//   }
+
+function changeTheme() {
+  if ($(".content").hasClass("bg-gradient-light")) {
+    $(".content").removeClass("bg-gradient-light");
+    $(".content").addClass("bg-gradient-dark");
+    $(".dark").hide();
+    $(".light").show();
+  } else {
+    $(".content").removeClass("bg-gradient-dark");
+    $(".content").addClass("bg-gradient-light");
+    $(".dark").show();
+    $(".light").hide();
+  }
+}
+
+//   document.addEventListener('DOMContentLoaded', function () {
+//     const sr = ScrollReveal({
+//         origin: 'bottom',
+//         distance: '50px',
+//         duration: 1000,
+//         reset: true
+//     });
+
+//     sr.reveal('.title', { interval: 200 });
+// });
+
+// // To handle partial load (like reload in the middle of the page)
+
+var slideUp = {
+  distance: "150%",
+  origin: "bottom",
+  opacity: 0,
+  duration: 1000,
+};
+
+let nodeArray = [
+  document.querySelector(".title"),
+  document.querySelector(".personal"),
+  document.querySelector(".info-details"),
+];
+// ScrollReveal().reveal(nodeArray, slideUp);
+ScrollReveal().reveal(".title", slideUp);
+ScrollReveal().reveal(".side-title", slideUp);
+ScrollReveal().reveal(".info-details", slideUp);
+ScrollReveal().reveal(".education-title", slideUp);
+ScrollReveal().reveal(".education", slideUp);
+ScrollReveal().reveal(".skill-container", slideUp);
+ScrollReveal().reveal(".service-container", slideUp);
+ScrollReveal().reveal(".project-container", slideUp);
+ScrollReveal().reveal(".contact-container", slideUp);
+
+(function () {
+  emailjs.init({
+    publicKey: "aREL8IDmHjOe_tCPL",
+  });
+})();
+
+let contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", () => {
+  console.log("clicked");
+  event.preventDefault();
+  let serviceID = "service_q58lxzi";
+  let templateID = "template_1tlfyox";
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs.send(serviceID, templateID, params).then(
+    (res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+    },
+    (error) => {
+      console.log("fail", error);
+    }
+  );
+});
+
+function opennav() {
+  $(".res-navigation").show();
+  $(".hamburger").hide();
+}
+
+function closenav() {
+  $(".res-navigation").hide();
+  $(".hamburger").show();
+}
+
+$(".nav-menu").click(function () {
+  closenav();
+});
+
+if (window.pageYOffset > 500) {
+  $(".top-scroll").removeClass("hidden");
+}
+window.onscroll = function () {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset > 500) {
+    $(".top-scroll").removeClass("hidden");
+  } else {
+    $(".top-scroll").addClass("hidden");
+  }
+};
+
+function toTop() {
+  $(window).scrollTop(0);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-light': 'linear-gradient(75deg, rgba(79,70,229,1) 22%, rgba(252,252,252,1) 22%)',
+        'gradient-dark': 'linear-gradient(75deg, rgba(79,70,229,1) 22%, rgba(0,0,0,1) 22%)',
+      },
+    },
+  },
+  plugins: [],
+}
+
