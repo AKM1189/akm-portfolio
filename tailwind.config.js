f1721b14c972efe26413ba30baa43f16104dba23
akm <109449422+AKM1189@users.noreply.github.com>
2025-05-31 15:39:11 +0630

    fix fav not found

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.{html,js}",
-    "./index.html"
-  ],
+  content: ["public/*.{html,js}", "./index.html"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-light': 'linear-gradient(75deg, rgba(79,70,229,1) 22%, rgba(252,252,252,1) 22%)',
-        'gradient-dark': 'linear-gradient(75deg, rgba(79,70,229,1) 22%, rgba(0,0,0,1) 22%)',
+        "gradient-light":
+          "linear-gradient(75deg, rgba(79,70,229,1) 22%, rgba(252,252,252,1) 22%)",
+        "gradient-dark":
+          "linear-gradient(75deg, rgba(79,70,229,1) 22%, rgba(0,0,0,1) 22%)",
       },
     },
   },
   plugins: [],
-}
-
+};
